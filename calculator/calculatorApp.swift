59b9d18c8ee39e705c3048d6c89416f7f7d437c4
erikalejandro.reyes.contractor <erikalejandro.reyes.contractor@bbva.com>
2020-09-19 00:54:32 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Reyes Lozano Erik Alejandro on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
